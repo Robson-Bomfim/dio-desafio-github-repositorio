11637a8ab877c7d32760aab057b7f4b44b41b4d7
Robson Bomfim <robsombomfim@gmail.com>
2022-01-09 22:35:59 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace dio_desafio_github_repositorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string userOption = Console.ReadLine();
+
+            while (userOption.ToUpper() != "X")
+            {
+                switch (userOption)
+                {
+                    case "1":
+                        Console.WriteLine("Inserir novo aluno");
+                        break;
+                    case "2":
+                        Console.WriteLine("Listar alunos");
+                        break;
+                    case "3":
+                        Console.WriteLine("Calcular média geral");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Opção invalida!");
+                }
+
+                Console.WriteLine("\nInforme a opção desejada: ");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                userOption = Console.ReadLine();
+            }
+        }
+    }
+}
